2ff970f22eba59210f5b8a2c65d0e23255f6f316
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-10 19:48:09 +0900

    - json-server 설정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
+    <header>
     <h1>{{ $route.name }} 페이지</h1>
+      <NuxtLink to="/">홈페이지</NuxtLink>
+      <NuxtLink to="/product">상품 페이지</NuxtLink>
+    </header>
   <Nuxt />
   </div>
 </template>
