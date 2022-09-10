38e0a8a4825d136fe3a3c4cee39ebb4f92fd89ec
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-10 21:11:11 +0900

    - eslint 오류 수정(eslintrc.js 파일을 .eslintrc 파일로 변경) - prettier 오류 수정(comma)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <header>
-    <h1>{{ $route.name }} 페이지</h1>
+      <h1>{{ $route.name }} 페이지</h1>
       <NuxtLink to="/">홈페이지</NuxtLink>
       <NuxtLink to="/product">상품 페이지</NuxtLink>
     </header>
-  <Nuxt />
+    <Nuxt />
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import Vue from 'vue'
+
+export default Vue.extend({
   created() {
-    console.log(this.$route)
-  }
-}
+    // console.log(this.$route)
+  },
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,15 +1,32 @@
 <script lang="ts">
+import Vue from 'vue'
 
+export default Vue.extend({
+  asyncData() {},
+  data() {
+    return {
+      products: [],
+    }
+  },
+
+  async created() {
+    // // vue 라이프 사이클 방식 - Nuxt 에서는 권장하지 않음
+    // const response = await axios.get('http://localhost:8080/products');
+    // console.log(response);
+    // this.products = response.data;
+  },
+})
 </script>
 
 <template>
   <ul>
+    <li v-for="item in products" :key="item.id">
+      {{ item.name }}
+    </li>
     <li>상품 1</li>
     <li>상품 2</li>
     <li>상품 3</li>
   </ul>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
