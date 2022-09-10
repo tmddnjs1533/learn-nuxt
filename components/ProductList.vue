0d343c5ba96d9ee9e265ea9a297f0f01cb6080c8
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-09-10 21:48:46 +0900

    - asyncData 적용

---
+++ components/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ProductList'
+  // page 컴포넌트가 아니면 사용할 수 없다.
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:8080/products')
+  //   const products = response.data
+  //   return { products }
+  // }
+})
+</script>
+
+<style scoped></style>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,31 +1,44 @@
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
-  asyncData() {},
-  data() {
-    return {
-      products: [],
-    }
-  },
+  /**
+   * 페이지 컴포넌트에만 제공되는 속성
+   * data에 할당하는 것이 아니고 반환함.
+   * VueRouter 의 beforeEnter() {} 와 동일(데이터 fetch 를 완료한 후에 사용자에게 페이지를 보여주는 속성)
+   */
+  async asyncData() {
+    const response = await axios.get('http://localhost:8080/products')
+    const products = response.data.map((item) => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`
+    }))
+    return { products }
+  }
 
-  async created() {
-    // // vue 라이프 사이클 방식 - Nuxt 에서는 권장하지 않음
-    // const response = await axios.get('http://localhost:8080/products');
-    // console.log(response);
-    // this.products = response.data;
-  },
+  // data() {
+  //   return {
+  //     products: []
+  //   }
+  // },
+
+  // async created() {
+  // // vue 라이프 사이클 방식 - Nuxt 에서는 권장하지 않음
+  // const response = await axios.get('http://localhost:8080/products');
+  // console.log(response);
+  // this.products = response.data;
+  // }
 })
 </script>
 
 <template>
   <ul>
-    <li v-for="item in products" :key="item.id">
-      {{ item.name }}
+    <li v-for="product in products" :key="product.id">
+      <img :src="product.imageUrl" :alt="product.name" />
+      <p>{{ product.name }}</p>
+      <p>{{ product.price }}</p>
     </li>
-    <li>상품 1</li>
-    <li>상품 2</li>
-    <li>상품 3</li>
   </ul>
 </template>
 
